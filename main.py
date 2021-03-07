cd3aae402998398997bae9e1fac3820e5e8cd7f5
lzangari <luca.zangari@me.com>
2021-03-07 22:50:37 +0100

    Testing lucid translation to tf2

---
+++ main.py
@@ -0,0 +1,49 @@
+import argparse
+from luna.pretrained_models import models
+from luna.featurevis import featurevis, images, image_reader
+import tensorflow as tf
+import numpy as np
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-a", "--architecture", type = str, default = "inceptionV3", help = "The model architecture")
+parser.add_argument("-l", "--layerName", type = str, default = "mixed6", help = "The chosen layer of the model architecture")
+parser.add_argument("-c", "--channelNum", type = int, default = 1, help = "The chosen channel index of the defined model architecture's layer ")
+args = parser.parse_args()
+
+arch = args.architecture
+layer_name = args.layerName
+channel_num = args.channelNum
+
+
+
+model = models.model_inceptionv3()
+#model = models.model_resnet50v2()
+#model = models.model_inceptionv1()
+#model = models.model_inceptionv1_slim()
+#model = models.model_vgg16()
+
+# model.summary()
+# model = models.model_alexnet()
+#image = images.initialize_image(224, 224)
+
+image = lambda: images.initialize_image_luna(224, fft=True, decorrelate=True)
+
+#image_old = images.initialize_image(224, 224)
+
+#image = tf.transpose(image(), [0,3, 1,2])
+
+
+print(np.shape(image()))
+
+#'inception_4b/output'
+#'mixed6'
+
+loss, image = featurevis.visualize_filter(image(), model, layer_name,
+                                          channel_num, 2000, 1, 0, True, 0, transforms=None)
+name = "feature_vis_{}_{}_{}".format(arch, layer_name.replace("/", "-"), channel_num)
+print(loss)
+
+images.save_image(image, name= name)
+image_reader.save_npy_as_png("{}.npy".format(name), "luna/outputs")
